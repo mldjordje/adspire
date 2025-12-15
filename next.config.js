96784938fb8933e8013a5a5631566af1cdce05f9
mldjordje <165197022+mldjordje@users.noreply.github.com>
2025-12-15 01:51:09 +0100

    Serve Kopex static site from /kopex path

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  async rewrites() {
+    return [
+      {
+        source: "/kopex",
+        destination: "/kopex/index.html",
+      },
+      {
+        source: "/kopex/",
+        destination: "/kopex/index.html",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
